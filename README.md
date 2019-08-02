5aacd9cd6233739692f3057cc6ea033136fcc4d5
Ben Evans <bevans@newrelic.com>
2019-08-02 10:27:27 +0000

    Add main

---
+++ README.md
@@ -0,0 +1,4 @@
+## Jackson Example
+
+Example project to work with Maven, Jackson and the Java 11 HTTP client
+ 
---
+++ src/main/java/glblcd/Main.java
@@ -0,0 +1,7 @@
+package glblcd;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
