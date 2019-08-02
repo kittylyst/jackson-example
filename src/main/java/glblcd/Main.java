7cfee7e833018904d02a4c82591a83d4a5d89019
Ben Evans <bevans@newrelic.com>
2019-08-02 13:44:30 +0000

    Fix security issue

--- src/main/java/glblcd/Main.java
+++ src/main/java/glblcd/Main.java
@@ -17,7 +17,6 @@ import java.util.Map;
 
 public class Main {
 
-
     public static void main(String[] args) {
         String location = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"; //args[0];
 
