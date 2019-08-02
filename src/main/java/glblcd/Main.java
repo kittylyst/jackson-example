bc119368bd3b641c6d83d1306582080f01f8c0ac
Ben Evans <bevans@newrelic.com>
2019-08-02 11:18:45 +0000

    Minimal test

--- src/main/java/glblcd/Main.java
+++ src/main/java/glblcd/Main.java
@@ -1,9 +1,21 @@
 package glblcd;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
 
     public static void main(String[] args) {
-
+        String json = null;
+        try {
+            byte[] contents = Files.readAllBytes(Paths.get("src/main/resources/cake.json"));
+            json = new String(contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println(json);
     }
 }
